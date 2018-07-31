44db52f3d70d2d62852932c2d8c3130570f6846c
Kitrov <justtheviktor@gmail.com>
2018-07-31 21:00:50 +0300

    Initial commit

---
+++ Solution/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+    internal class Program
+    {
+        private static List<Result> findResults(List<List<int>> inputList)
+        {
+            List<Result> results = new List<Result>();
+            // find all result combination
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    PyramidUtils.generateFirstResults(inputList[i], results);
+                }
+                else
+                {
+                    List<Result> newResults = new List<Result>();
+                    foreach (Result result in results)
+                    {
+                        List<Result> tempResults = PyramidUtils.findNextNumbersResults(inputList[i], result);
+                        if (tempResults != null)
+                        {
+                            newResults.AddRange(tempResults);
+                        }
+                    }
+
+                    results.Clear();
+
+                    results.AddRange(newResults);
+                }
+            }
+
+            return results;
+        }
+
+        private static Result findMaxResult(List<Result> results)
+        {
+            int max = 0;
+            int maxIndex = -1;
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (maxIndex == -1 || max < results[i].Sum)
+                {
+                    max = results[i].Sum;
+                    maxIndex = i;
+                }
+            }
+
+            if (maxIndex != -1)
+            {
+                return results[maxIndex];
+            }
+
+            return null;
+        }
+
+        private static void printResult(Result result)
+        {
+            if (result != null)
+            {
+                Console.WriteLine("Max sum: " + result.Sum);
+                Console.WriteLine("Path: " + result.getRoute());
+            }
+            else
+            {
+                Console.WriteLine("Neither path reached bottom of pyramid.");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            List<List<int>> inputList = ReadUtils.readInputFromFile(@"..\..\input.txt");
+            Console.WriteLine();
+            Console.WriteLine("Output:");
+            printResult(findMaxResult(findResults(inputList)));
+        }
+    }
+}
---
+++ Solution/Result.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Solution
+{
+    public class Result
+    {
+        private int sum;
+        private List<int> path;
+        private int fatherIndex = -1;
+
+        public Result()
+        {
+            sum = 0;
+            path = new List<int>();
+        }
+
+        public void addPoint(int point)
+        {
+            path.Add(point);
+            sum += point;
+        }
+
+        public int Sum
+        {
+            get { return sum; }
+        }
+
+        public string getRoute()
+        {
+            string route = "";
+            foreach (int point in path)
+            {
+                route = route + point + ", ";
+            }
+
+            return route.Remove(route.Length - 2, 2);
+        }
+
+        public int FatherIndex
+        {
+            get { return fatherIndex; }
+            set { fatherIndex = value; }
+        }
+
+        public bool isLastPointEven()
+        {
+            return PyramidUtils.isEven(path[path.Count - 1]);
+        }
+
+        public Result clone()
+        {
+            Result result = new Result();
+            result.FatherIndex = FatherIndex;
+            foreach (int point in path)
+            {
+                result.addPoint(point);
+            }
+
+            return result;
+        }
+
+    }
+}
